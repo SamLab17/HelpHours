7a043b1afcac3a2122720ff1c935a2c90213f139
Tony <tchang144@gmail.com>
2020-08-03 22:12:23 -0500

    Break up tests

--- tests/student_tests.py
+++ tests/student_tests.py
@@ -25,32 +25,5 @@ class TestStudent(unittest.TestCase):
         self.assertEqual("jane@gmail.com", x.email)
         self.assertEqual("jd1234", x.eid)
 
-class TestRoute(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
-    def tearDown(self):
-        pass
-
-    def test_main_page(self):
-        response = self.app.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_join_page(self):
-        response = self.app.get('/join', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_page(self):
-        response = self.app.get('/view', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_remove_page(self):
-        response = self.app.get('/remove', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_login_page(self):
-        response = self.app.get('/login', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
 if __name__ == '__main__':
     unittest.main()
