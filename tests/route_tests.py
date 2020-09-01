ddf8ed23e3d0e6894cdc1d5ebf64c4343f92b28a
Tony <51179409+ClosingBracket@users.noreply.github.com>
2020-09-01 01:28:52 -0500

    Add more route tests
    Add more of the routes that exist on our webapp for our CircleCI tool to test.

--- tests/route_tests.py
+++ tests/route_tests.py
@@ -15,6 +15,11 @@ class TestRoute(unittest.TestCase):
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+        
+    # should be same as main page
+    def test_index_page(self):
+        response = self.app.get('/index', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_join_page(self):
         response = self.app.get('/join', follow_redirects=True)
@@ -28,9 +33,34 @@ class TestRoute(unittest.TestCase):
         response = self.app.get('/remove', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    def test_zoom_redirect(self):
+        response = self.app.get('/zoom', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_schedule_redirect(self):
+        response = self.app.get('/schedule', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_about_page(self):
+        response = self.app.get('/about', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
     def test_login_page(self):
         response = self.app.get('/login', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+        
+    # unauthorized routes by default
+    def test_stats_page(self):
+        response = self.app.get('/stats', follow_redirects=True)
+        self.assertEqual(response.status_code, 401)
+        
+    def test_change_zoom_page(self):
+        response = self.app.get('/change_zoom', follow_redirects=True)
+        self.assertEqual(response.status_code, 401)
+
+    def test_admin_panel(self):
+        response = self.app.get('/admin_panel', follow_redirects=True)
+        self.assertEqual(response.status_code, 401)
 
 
 if __name__ == '__main__':
