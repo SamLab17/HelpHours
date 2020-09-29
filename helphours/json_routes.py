a06c4ac8558f86cf7712d9ca581d21b37fa6abdc
Samuel Laberge <samlaberge@live.com>
2020-09-28 21:20:30 -0600

    Fix cached queue response bug

--- helphours/json_routes.py
+++ helphours/json_routes.py
@@ -6,7 +6,11 @@ from flask_login import current_user
 @app.route("/queue", methods=['GET'])
 def queue():
     """ Returns a JSON representation of the queue """
-    return queue_handler.get_serialized_queue(instructorView=current_user.is_authenticated)
+    queue_json = queue_handler.get_serialized_queue(instructorView=current_user.is_authenticated)
+    return app.response_class(
+        response=queue_json,
+        mimetype='application/json'
+    )
 
 
 @app.route("/queue_status", methods=['GET'])
