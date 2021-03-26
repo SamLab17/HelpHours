4f09e26a49f4b4560e29cf95c9a2bdd485108691
Samuel Laberge <samlaberge@live.com>
2021-03-26 00:01:38 -0500

    rave

--- helphours/static/scripts/darkmode.js
+++ helphours/static/scripts/darkmode.js
@@ -1,32 +1,116 @@
 const store = window.localStorage;
+const bodyEl = document.getElementById('body');
+
+// Rave variables
+let i = 0;
+const msPerFrame = 250;
+const contentDiv = document.getElementById('body');
+const raveDuckImg = '/static/images/rave_duck.gif';
+const navDuck = document.getElementById('nav-duck');
+const ogDuckImg = navDuck.getAttribute('src');
+const amplitude = 100;
+const shift = 255 - amplitude;
+let step = 1.7 * (msPerFrame / 1000);
+let interval = NaN;
+
+if(store.getItem('raveMode') === 'true') {
+    enableRave();
+} else {
+    disableRave();
+}
 
 if (store.getItem('darkMode') === 'true') {
-    let bodyEl = document.getElementById('body');
-    bodyEl.classList.add('dark');
-    store.setItem('darkMode', true)
-    document.getElementById('dark-mode-moon').style.display = "none";
-    document.getElementById('dark-mode-sun').style.display = "block";
-    // Don't show a transition when the page loads
     bodyEl.classList.add('no-transition');
+    setDarkMode();
     setTimeout(() => {bodyEl.classList.remove('no-transition')})
+    // Don't show a transition when the page loads
 } else {
     document.getElementById('dark-mode-moon').style.display = "block";
     document.getElementById('dark-mode-sun').style.display = "none";
 }
 
 function toggleDarkMode() {
-    let bodyEl = document.getElementById('body');
     if (bodyEl.classList.contains('dark')) {
         // Change to light mode
-        bodyEl.className = '';
-        store.setItem('darkMode', false)
-        document.getElementById('dark-mode-moon').style.display = "block";
-        document.getElementById('dark-mode-sun').style.display = "none";
+        setLightMode(); 
     } else {
         // Change to dark mode
-        bodyEl.className = 'dark';
-        store.setItem('darkMode', true)
-        document.getElementById('dark-mode-moon').style.display = "none";
-        document.getElementById('dark-mode-sun').style.display = "block";
+        setDarkMode(); 
     }
 }
+
+function setLightMode() {
+    if(!bodyEl.classList.contains('dark'))
+        return;
+    store.setItem('darkMode', false)
+    document.getElementById('dark-mode-moon').style.display = "block";
+    document.getElementById('dark-mode-sun').style.display = "none";
+    bodyEl.classList.remove('dark');
+}
+
+function setDarkMode() {
+    if(bodyEl.classList.contains('dark'))
+        return;
+    store.setItem('darkMode', true)
+    document.getElementById('dark-mode-moon').style.display = "none";
+    document.getElementById('dark-mode-sun').style.display = "block";
+    bodyEl.classList.add('dark');
+    disableRave();
+}
+
+
+
+function toggleRaveMode() {
+    let bodyEl = document.getElementById('body');
+    bodyEl.classList.toggle('rave');
+    if (bodyEl.classList.contains('rave')) {
+       enableRave();
+    } else {
+        disableRave(); 
+    }
+}
+
+
+function enableRave() {
+    if(interval != NaN)
+        window.clearInterval(interval);
+        store.setItem('raveMode', true);
+    interval = window.setInterval(() => {
+            let red = Math.sin(step * i + 0) * amplitude + shift;
+            let green = Math.sin(step * i + 2.0 * Math.PI / 3) * amplitude + shift;
+            let blue = Math.sin(step * i + 4.0 * Math.PI / 3) * amplitude + shift;
+            contentDiv.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+            i++;
+    }, msPerFrame);
+    navDuck.setAttribute('src', raveDuckImg);
+    const leftHomeDuck = document.getElementById('left-home-duck');
+    if(leftHomeDuck){
+        leftHomeDuck.setAttribute('src', raveDuckImg);
+        const rightHomeDuck = document.getElementById('right-home-duck');
+        if(rightHomeDuck){
+            // should always be !null
+            rightHomeDuck.setAttribute('src', raveDuckImg);
+        }
+    }
+    setLightMode();
+}
+
+function disableRave() {
+    if(interval == NaN)
+        return;
+    store.setItem('raveMode', false);
+    contentDiv.style.backgroundColor = "";
+    window.clearInterval(interval);
+    navDuck.setAttribute('src', ogDuckImg);
+    const leftHomeDuck = document.getElementById('left-home-duck');
+    if(leftHomeDuck){
+        leftHomeDuck.setAttribute('src', ogDuckImg);
+        const rightHomeDuck = document.getElementById('right-home-duck');
+        if(rightHomeDuck){
+            // should always be !null
+            rightHomeDuck.setAttribute('src', ogDuckImg);
+        }
+    }
+    interval = NaN;
+}
+
