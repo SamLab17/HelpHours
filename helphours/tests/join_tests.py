30cdb0d8b0b4e9b4b7ceaff5d02a27804ac2eeec
SamLab17 <samlaberge@live.com>
2021-09-13 19:29:35 -0500

    join test

--- helphours/tests/join_tests.py
+++ helphours/tests/join_tests.py
@@ -6,11 +6,13 @@ import json
 class JoinTests(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
+        # See "open" token in config
+        self.app.post('/open', data={'token': '312'})
 
     def tearDown(self):
         pass
 
-    def simple_join(self):
+    def test_simple_join(self):
         post_data = {
             'name': 'Alice',
             'email': 'example@example.com',
@@ -18,7 +20,7 @@ class JoinTests(unittest.TestCase):
             'desc': 'Testing problem description',
             'modality': 'virtual'
         }
-        join_resp = self.app.post('/join', data=post_data)
+        join_resp = self.app.post('/join', data=post_data, follow_redirects=True)
         self.assertEqual(join_resp.status_code, 200)
         queue_resp = self.app.get('/queue')
         queue = json.loads(next(queue_resp.response))['queue']
